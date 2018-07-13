fc8a80888b5f875ea9442f74e88bdbda5fe6b5c7
Braden Marr <marr@pde.com>
2018-07-13 13:10:40 -0700

    Cleaned up edif_parse_lib.py to better meet pylint rules.  - removed space trailing (  - removed space leading )  - added spaces surrounding ==, !=, <, >, =  - removed unnecessary brackets () around if statements

--- src/edif_parse_lib.py
+++ src/edif_parse_lib.py
@@ -19,26 +19,26 @@ def extract_offset_connections(library_Component, port_impl_list, port_list, off
     for port_impl in port_impl_list:
 
         i_name = port_impl.get_object("name")
-        if (i_name!=None):
+        if i_name != None:
             port_impl_p_name = i_name.get_param(0)
 
-            if (port_impl_p_name!=None):
+            if port_impl_p_name != None:
                 #print "port_impl_p_name =", port_impl_p_name
 
                 pin_name, pin_type, pin_number = extract_pin_parameters(port_impl_p_name, port_list)
                 #print pin_name, pin_type, pin_number
 
-                if (pin_number!=None):
+                if pin_number != None:
                     connection = KicadConnection(pin_number, pin_name)
                     connection.set_offset(offset_x, offset_y)
                     dot_pt = port_impl.get_object("connectLocation.figure.dot.pt")
-                    if (dot_pt!=None):
-                        dot_x, dot_y = convert_kicad_coor( extract_edif_pt(dot_pt) )
+                    if dot_pt != None:
+                        dot_x, dot_y = convert_kicad_coor(extract_edif_pt(dot_pt))
 
                     ptl_pin = port_impl.get_object("figure.path.pointList")
-                    if (ptl_pin!=None):
-                        x1, y1 = convert_kicad_coor( extract_edif_pt(ptl_pin.get_param(0)) )
-                        x2, y2 = convert_kicad_coor( extract_edif_pt(ptl_pin.get_param(1)) )
+                    if ptl_pin != None:
+                        x1, y1 = convert_kicad_coor(extract_edif_pt(ptl_pin.get_param(0)))
+                        x2, y2 = convert_kicad_coor(extract_edif_pt(ptl_pin.get_param(1)))
                         # x1 = dot_x
                         # y1 = dot_y
                         connection.set_pin(x1, y1, x2, y2)
@@ -55,7 +55,7 @@ def extract_offset_point_list(library_Component, point_list, offset_x, offset_y)
         poly.set_offset(offset_x, offset_y)
         pts = search_edif_objects(ptl, "pt")
         for pt in pts:
-            x, y = convert_kicad_coor( extract_edif_pt(pt) )
+            x, y = convert_kicad_coor(extract_edif_pt(pt))
             poly.add_segment(x, y)
         library_Component.add_draw(poly)
     return
@@ -69,7 +69,7 @@ def extract_offset_arc_point_list(library_Component, point_list, offset_x, offse
         arc.set_offset(offset_x, offset_y)
         pts = search_edif_objects(ptl, "pt")
         for pt in pts:
-            x, y = convert_kicad_coor( extract_edif_pt(pt) )
+            x, y = convert_kicad_coor(extract_edif_pt(pt))
             arc.add_point(x, y)
         library_Component.add_draw(arc)
     return
@@ -92,30 +92,30 @@ def extract_offset_drawing(library_Component, figure_list, offset_x, offset_y):
 
     for figure in figure_list:
         p1 = extract_edif_str_param(figure, 0)
-        if (p1!=None):
+        if p1 != None:
             path_list = search_edif_objects(figure, "path")
             extract_offset_path(library_Component, path_list, offset_x, offset_y)
 
         point_list = figure.get_object("polygon.pointList")
-        if (point_list!=None):
+        if point_list != None:
             extract_offset_point_list(library_Component, [point_list], offset_x, offset_y)
 
         arc_point_list = figure.get_object("openShape.curve.arc")
-        if (arc_point_list!=None):
+        if arc_point_list != None:
             extract_offset_arc_point_list(library_Component, [arc_point_list], offset_x, offset_y)
 
         rectangle = figure.get_object("rectangle")
-        if (rectangle!=None):
-            x1, y1 = convert_kicad_coor( extract_edif_pt(rectangle.get_param(0)) )
-            x2, y2 = convert_kicad_coor( extract_edif_pt(rectangle.get_param(1)) )
+        if rectangle != None:
+            x1, y1 = convert_kicad_coor(extract_edif_pt(rectangle.get_param(0)))
+            x2, y2 = convert_kicad_coor(extract_edif_pt(rectangle.get_param(1)))
             rectangle = KicadRectangle(x1, y1, x2, y2)
             rectangle.set_offset(offset_x, offset_y)
             library_Component.add_draw(rectangle)
 
         circle = figure.get_object("circle")
-        if (circle!=None):
-            x1, y1 = convert_kicad_coor( extract_edif_pt(circle.get_param(0)))
-            x2, y2 = convert_kicad_coor( extract_edif_pt(circle.get_param(1)))
+        if circle != None:
+            x1, y1 = convert_kicad_coor(extract_edif_pt(circle.get_param(0)))
+            x2, y2 = convert_kicad_coor(extract_edif_pt(circle.get_param(1)))
             circle = KicadCircle(x1, y1, x2, y2)
             circle.set_offset(offset_x, offset_y)
             library_Component.add_draw(circle)
@@ -131,20 +131,20 @@ def extract_pin_parameters(port_impl_p_name, port_list):
 
     for port in port_list:
 
-        if ( extract_edif_str_param(port, 0)[0]==port_impl_p_name):
+        if extract_edif_str_param(port, 0)[0] == port_impl_p_name:
             #print port_impl_p_name, "found"
             properties = search_edif_objects(port, "property")
 
             for property in properties:
-                p1_name = remove_quote( extract_edif_str_param(property, 0)[1] )
-                string = remove_quote( property.get_object("string").get_param(0) )
+                p1_name = remove_quote(extract_edif_str_param(property, 0)[1])
+                string = remove_quote(property.get_object("string").get_param(0))
                 #print p1_name, ":", string
 
-                if (p1_name=="Name"):
+                if p1_name == "Name":
                     pin_name = string
-                elif (p1_name=="Type"):
+                elif p1_name == "Type":
                     pin_type = string
-                elif (p1_name=="PackagePortNumbers"):
+                elif p1_name == "PackagePortNumbers":
                     pin_number = string
 
     return [pin_name, pin_type, pin_number]
@@ -158,14 +158,14 @@ def find_edif_points_maxmin(point_list, maxmin_xy):
     for point in point_list:
         pts = search_edif_objects(point, "pt")
         for pt in pts:
-            x, y = convert_kicad_coor( extract_edif_pt(pt) )
-            if (x > max_x):
+            x, y = convert_kicad_coor(extract_edif_pt(pt))
+            if x > max_x:
                 max_x = x
-            if (y > max_y):
+            if y > max_y:
                 max_y = y
-            if (x < min_x):
+            if x < min_x:
                 min_x = x
-            if (y < min_y):
+            if y < min_y:
                 min_y = y
 
     return [max_x, max_y], [min_x, min_y]
@@ -182,27 +182,27 @@ def extract_powerobject_symbol(library_Component, name, figure_list):
 
     figure = figure_list[0]
     figureGroupOverride = figure.get_object("figureGroupOverride")
-    if (figureGroupOverride!=None):
+    if figureGroupOverride != None:
         figure_name = figureGroupOverride.get_param(0)
     else:
         figure_name = figure.get_param(0)
 
-    if (figure_name=="POWEROBJECT"):
+    if figure_name == "POWEROBJECT":
         ref = "#PWR"
         max_xy = [max_x, max_y]
         min_xy = [min_y, min_y]
         for figure in figure_list:
             path_list = search_edif_objects(figure, "path")
-            if (len(path_list)!=0):
+            if len(path_list) != 0:
                 for path in path_list:
                     point_list = search_edif_objects(path, "pointList")
                     max_xy, min_xy = find_edif_points_maxmin(point_list, [max_xy, min_xy])
             circle = search_edif_objects(figure, "circle")
-            if (len(circle)!=0):
+            if len(circle) != 0:
                 print " found circle in POWEROBJECT"
                 max_xy, min_xy = find_edif_points_maxmin(circle, [max_xy, min_xy])
             rectangle = search_edif_objects(figure, "rectangle")
-            if (len(rectangle)!=0):
+            if len(rectangle) != 0:
                 print " found rect in POWEROBJECT"
                 max_xy, min_xy = find_edif_points_maxmin(rectangle, [max_xy, min_xy])
 
@@ -214,7 +214,7 @@ def extract_powerobject_symbol(library_Component, name, figure_list):
 
         connection = KicadConnection("1", name)
         x_offset = -(min_x + (max_x - min_x) / 2)
-        if (name in set(['GND', 'DGND', 'AGND', 'EARTH', 'GND_POWER'])):
+        if name in set(['GND', 'DGND', 'AGND', 'EARTH', 'GND_POWER']):
             #y_offset = -100 # negative number works
             y_offset = 0
             #y_offset = -(min_y + (max_y - min_y) / 2)
@@ -244,7 +244,7 @@ def extract_powerobject_symbol(library_Component, name, figure_list):
 
 def _extract_component_view(view, library_Component):
     view_name = extract_edif_str_param(view, 0)
-    ref="?"
+    ref = "?"
     x = int(0)
     y = int(0)
     value_x = int(0)
@@ -254,36 +254,36 @@ def _extract_component_view(view, library_Component):
     hvjustify = text_justify("CENTERCENTER", "R0", "R0")
 
     interface = view.get_object("interface")
-    if (interface!=None):
+    if interface != None:
         symbol  = interface.get_object("symbol")
-        if (symbol!=None):
+        if symbol != None:
             property_list = search_edif_objects(symbol, "property")
-            if (property_list!=None):
+            if property_list != None:
                 for property in property_list:
                     p1 = extract_edif_str_param(property, 0)[0]
-                    if (p1=="VALUE"):
+                    if p1 == "VALUE":
                         string = property.get_object("string")
-                        if (string!=None):
+                        if string != None:
                             stringDisplay = string.get_object("stringDisplay")
-                            if (stringDisplay!=None):
+                            if stringDisplay != None:
                                 #value = normalize_edif_string(str(stringDisplay.get_param(0)))
                                 value = stringDisplay.get_param(0)
                                 pt = stringDisplay.get_object("display.origin.pt")
-                                if (pt!=None):
-                                    value_x, value_y = convert_kicad_coor( extract_edif_pt(pt) )
+                                if pt != None:
+                                    value_x, value_y = convert_kicad_coor(extract_edif_pt(pt))
                             else:
                                 print "ASSERT: unexpected missing stringDisplay in EDIF as " + str(extract_edif_str_param(string, 0)[0])
-                    elif (p1=="PIN_NAMES_VISIBLE"):
+                    elif p1 == "PIN_NAMES_VISIBLE":
                         string = property.get_object("string")
-                        if (string!=None):
+                        if string != None:
                             pin_names_visible = remove_quote(extract_edif_str_param(string, 0)[1])
-                            if (pin_names_visible=="False"):
+                            if pin_names_visible == "False":
                                 library_Component.set_pin_names_visible(False)
-                    elif (p1=="PIN_NUMBERS_VISIBLE"):
+                    elif p1 == "PIN_NUMBERS_VISIBLE":
                         string = property.get_object("string")
-                        if (string!=None):
+                        if string != None:
                             pin_numbers_visible = remove_quote(extract_edif_str_param(string, 0)[1])
-                            if (pin_numbers_visible=="False"):
+                            if pin_numbers_visible == "False":
                                 library_Component.set_pin_numbers_visible(False)
 
             figure_list = search_edif_objects(symbol, "figure")
@@ -294,48 +294,48 @@ def _extract_component_view(view, library_Component):
             extract_connections(library_Component, port_impl_list, port_list)
 
         designator = interface.get_object("designator")
-        if (designator!=None):
+        if designator != None:
             #print "designator = "+extract_str_param(designator, 0)[1]
             ref = extract_edif_str_param(designator, 0)[1]
             ref = remove_quote(ref)
-            if (ref.endswith('?')):
+            if ref.endswith('?'):
                 ref = ref[:-1]
             stringDisplay = designator.get_object("stringDisplay")
-            if (stringDisplay!=None):
+            if stringDisplay != None:
                 print "Info: found extra information in reference designator:"
                 pt = stringDisplay.get_object("display.origin.pt")
-                if (pt!=None):
+                if pt != None:
                     print "  " + str(extract_edit_pt(pt))
-#                   x, y = convert_kicad_coor( extract_edif_pt(pt) )
+#                   x, y = convert_kicad_coor(extract_edif_pt(pt))
 
 #               display_justify = stringDisplay.get_object("display.justify")
-#               if (display_justify!=None):
+#               if display_justify != None:
 #                   hvjustify = text_justify(display_justify.get_param(0), "R0")
 #                   print "JUSTIFY with " + str(hvjustify)
             else:
                 pt = interface.get_object("symbol.keywordDisplay.display.origin.pt")
-                if (pt!=None):
-                    x, y = convert_kicad_coor( extract_edif_pt(pt) )
+                if pt != None:
+                    x, y = convert_kicad_coor(extract_edif_pt(pt))
 
                 display_justify = interface.get_object("symbol.keywordDisplay.display.justify")
-                if (display_justify!=None):
+                if display_justify != None:
                     hvjustify = text_justify(display_justify.get_param(0), "R0", "R0")
                     #print "JUSTIFY with " + str(hvjustify)
 
     contents =  view.get_object("contents")
-    if (contents!=None):
+    if contents != None:
         figure_list = search_edif_objects(contents, "figure")
         figure = figure_list[0]
         figureGroupOverride = figure.get_object("figureGroupOverride")
-        if (figureGroupOverride!=None):
+        if figureGroupOverride != None:
             figure_name = figureGroupOverride.get_param(0)
         else:
             figure_name = figure.get_param(0)
 
-        if (figure_name=="POWEROBJECT"):
+        if figure_name == "POWEROBJECT":
             power_symbol = extract_powerobject_symbol(library_Component, view_name[0], figure_list)
-            if (power_symbol['is_valid']==True):
-                if (power_symbol['is_ground']==True):
+            if power_symbol['is_valid'] == True:
+                if power_symbol['is_ground'] == True:
                     #print "**** GND"
                     x = 0
                     y = power_symbol['min_y']
@@ -349,9 +349,10 @@ def _extract_component_view(view, library_Component):
                     value_y = power_symbol['max_y'] + 100
                 visible = "V"
                 orientation = "V"
-                ref="#PWR"
+                ref = "#PWR"
                 library_Component.set_pin_names_visible(False)
                 library_Component.set_pin_numbers_visible(False)
+    # pylint: disable=W0105
     """
     #
     # GND
@@ -418,7 +419,7 @@ def extract_kicad_component_view_library(view, cell_name, view_index):
 
     library_Component = KicadLibraryComponent(view_name[0])
 
-    library_Component.add_alias(cell_name[0] + "_" + chr( ord('A') + view_index))
+    library_Component.add_alias(cell_name[0] + "_" + chr(ord('A') + view_index))
 
     _extract_component_view(view, library_Component)
 
@@ -430,7 +431,7 @@ def extract_kicad_library(kicad_library, edif_library):
     # the rename tag, otherwise use the parameter of the
     # library tag
     rename = edif_library.get_object("rename")
-    if (rename!=None):
+    if rename != None:
         library_name = rename.get_param(0)
     else:
         library_name = edif_library.get_param(0)
@@ -440,14 +441,14 @@ def extract_kicad_library(kicad_library, edif_library):
 
     for edif_cell in cells:
         view_list = search_edif_objects(edif_cell, "view")
-        if (len(view_list) > 1):
+        if len(view_list) > 1:
             cell_name_as_alias = extract_edif_str_param(edif_cell, 0)
             print "multiview component: " + cell_name_as_alias[0] + " creates new ones"
             for view in view_list:
                 library_Component = extract_kicad_component_view_library(view, cell_name_as_alias, view_list.index(view))
-                kicad_library.add_component( library_Component )
+                kicad_library.add_component(library_Component)
         else:
             library_Component = extract_kicad_component_library(edif_cell)
-            kicad_library.add_component( library_Component )
+            kicad_library.add_component(library_Component)
 
     return kicad_library
